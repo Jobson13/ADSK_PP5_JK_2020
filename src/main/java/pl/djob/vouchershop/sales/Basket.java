2e3b3500b1f6c155214786b7a3f1e011a26e184b
Jobson13 <50521404+Jobson13@users.noreply.github.com>
2021-01-24 10:30:43 +0100

    All Works

--- src/main/java/pl/djob/vouchershop/sales/Basket.java
+++ src/main/java/pl/djob/vouchershop/sales/Basket.java
@@ -2,16 +2,18 @@ package pl.djob.vouchershop.sales;
 
 import pl.djob.vouchershop.catalog.Product;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Basket {
     private final Map<String, Product> products;
+    private final Map<String, Integer> productsQuantities;
 
     public Basket() {
         this.products = new HashMap<>();
+        this.productsQuantities = new HashMap<>();
     }
 
     public boolean isEmpty() {
@@ -19,7 +21,26 @@ public class Basket {
     }
 
     public void add(Product product) {
+        if (isInBasket(product)) {
+            increaseQuantity(product);
+        } else {
+            putIntoBasket(product);
+        }
+    }
+
+    private void putIntoBasket(Product product) {
+        productsQuantities.put(product.getId(), 1);
         products.put(product.getId(), product);
+    }
+
+    private void increaseQuantity(Product product) {
+        productsQuantities.put(
+                product.getId(),
+                productsQuantities.get(product.getId()) + 1);
+    }
+
+    private boolean isInBasket(Product product) {
+        return productsQuantities.containsKey(product.getId());
     }
 
     public Integer getProductsCount() {
@@ -27,7 +48,10 @@ public class Basket {
     }
 
     public List<BasketItem> getBasketItems() {
-        return Collections.emptyList();
+        return productsQuantities.entrySet()
+                .stream()
+                .map(es -> new BasketItem(es.getKey(), es.getValue()))
+                .collect(Collectors.toList());
     }
 
     public void remove(String id) {
